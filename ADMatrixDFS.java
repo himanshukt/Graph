00a7030c28ae66ba888b7732ff5e1326ac9ee8d5
himanshu thakur <31796645+thimanshu98@users.noreply.github.com>
2020-02-25 13:27:13 +0530

    Create ADMatrixDFS.java

---
+++ ADMatrixDFS.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class bfs{
+	void transversal(int arr[][]) {
+		int v=arr.length;
+		Boolean visited[]=new Boolean[v];
+		Arrays.fill(visited,false);
+		Queue<Integer> que=new LinkedList<Integer>();
+		que.add(0);
+		visited[0]=true;
+		while(!que.isEmpty()) {
+			int j=que.poll();
+			System.out.println(j+1);
+			for(int i=0;i<v;i++) {
+				if(arr[j][i]==1 && visited[i]==false) {
+					que.add(i);
+					visited[i]=true;
+				}
+			}
+		}
+	}	
+}
+public class BFSMatrix {
+
+	public static void main(String[] args) {
+		int arr[][]= { {0,1,0,1,1},{1,0,1,1,0},{0,1,0,0,0},{1,1,0,0,1},{1,0,0,1,0}};
+		bfs obj=new bfs();
+		obj.transversal(arr);
+	}
+		
+}
